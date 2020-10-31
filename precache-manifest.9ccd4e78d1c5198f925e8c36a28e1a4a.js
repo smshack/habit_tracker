1124a36c3a3d84c4af71afc98623aa238c493c15
smshack <5432tat@naver.com>
2020-10-31 22:18:55 +0900

    Updates

---
+++ precache-manifest.9ccd4e78d1c5198f925e8c36a28e1a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb911423c6d1e51af9c6dd84559a3048",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "808f336e175550994073",
+    "url": "/habit_tracker/static/css/main.277c47a6.chunk.css"
+  },
+  {
+    "revision": "a915e9d2730f9a017c34",
+    "url": "/habit_tracker/static/js/2.aa543aad.chunk.js"
+  },
+  {
+    "revision": "76aa3a6712dcb2b7248a77b49e0cd963",
+    "url": "/habit_tracker/static/js/2.aa543aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808f336e175550994073",
+    "url": "/habit_tracker/static/js/main.77ff8405.chunk.js"
+  },
+  {
+    "revision": "dbb297bb1ca874a80b51",
+    "url": "/habit_tracker/static/js/runtime-main.bec78f78.js"
+  }
+]);
